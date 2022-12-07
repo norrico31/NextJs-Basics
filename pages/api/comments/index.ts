02b1d1b489738bb4ccf882748e99ffc8250bdb4b
norrico31 <norricobiason31@gmail.com>
2022-12-07 13:38:29 +0800

    post request

--- pages/api/comments/index.ts
+++ pages/api/comments/index.ts
@@ -2,5 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { comments } from "../../../data/comments";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(comments)
+    if (req.method === 'GET') {
+        res.status(200).json(comments)
+    } else if (req.method === 'POST') {
+        const comment = req.body.comment
+        comments.push({ id: Date.now() + 1 + '', text: comment })
+        res.status(201).json(comments)
+    }
 }
--- pages/comments/index.tsx
+++ pages/comments/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link"
 
 interface IComments {
@@ -7,6 +7,7 @@ interface IComments {
 
 function Comments() {
     const [comments, setComments] = useState<IComments[]>([])
+    const [comment, setComment] = useState<string>('')
     async function fetchComments() {
         try {
             const res = await fetch(`http://localhost:3000/api/comments`)
@@ -16,10 +17,23 @@ function Comments() {
             return error
         }
     }
+    async function addComment() {
+        try {
+            const res = await fetch(`http://localhost:3000/api/comments`, { method: 'POST', body: JSON.stringify({ comment }), headers: { 'Content-Type': "application/json" } })
+            const data = await res.json()
+            // setComments(data)
+            console.log(data)
+        } catch (error) {
+            return error
+        }
+    }
     return (
         <>
             <h1>Comments List</h1>
             <button onClick={fetchComments}>Fetch Comments</button>
+            <hr />
+            <input type="text" value={comment} onChange={(evt: React.ChangeEvent<HTMLInputElement>) => setComment(evt.target.value)} />
+            <button onClick={addComment}>Submit Comment</button>
             {comments?.map((comment: IComments) => (
                 <Link href={'/comments/' + comment.id} key={comment.id}>
                     <h2>{comment.id}</h2>
