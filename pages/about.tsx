75fd42fd55381762a51cdc0e13ecf53edb2640a5
norrico31 <norricobiason31@gmail.com>
2022-12-22 16:08:31 +0800

    bootstrap sample

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+    return (
+        <button className='btn btn-success'>Button</button>
+    )
+}
+
+export default about
---
+++ pages/contact.tsx
@@ -0,0 +1,7 @@
+const contact = () => {
+    return (
+        <h2>Contact Page</h2>
+    )
+}
+
+export default contact
