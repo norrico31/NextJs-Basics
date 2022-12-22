98ac42467a21183518e6dd194838748a1e78c06c
norrico31 <norricobiason31@gmail.com>
2022-12-22 17:08:41 +0800

    component level styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
+import styles from '../styles/Home.module.css'
 
 function Home() {
 	return (
-		<div>
-			<h1>TypeScript pre-rendering</h1>
+		<div className={styles.container}>
+			<h1 className={`${styles.hightlight} ${styles.outlineRed}`}>TypeScript pre-rendering</h1>
 			<Link href='/users'>Users</Link>
 			<br />
 			<Link href='/posts'>Posts</Link>
