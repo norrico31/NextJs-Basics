5df1557cc74b5f387e99619c5019975994a7447d
norrico31 <norricobiason31@gmail.com>
2022-12-25 18:50:05 +0800

    blurDataUrl don't have answer

---
+++ my-app/pages/pets.tsx
@@ -0,0 +1,17 @@
+import Img from 'next/image'
+import img1 from '../public/img/1.jpg'
+
+// blurDataUrl for dynamic images
+
+export default function pets() {
+    return (
+        <div>
+            <Img src={img1} alt='sample photos' placeholder='blur' width='280' height='480' />
+            {['1', '2', '3'].map(path => (
+                <div key={path}>
+                    <Img src={`/img/${path}.jpg`} blurDataURL='' alt={path} width='280' height='480' />
+                </div>
+            ))}
+        </div>
+    )
+}
