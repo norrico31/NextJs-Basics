1264d223368200fa5c50eea5b937ccc6d872cf21
norrico31 <norricobiason31@gmail.com>
2022-12-29 16:36:09 +0800

    assignment!. learn complex matching redirect in next.js docs

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: false
+      },
+      {
+        source: '/old-blog/:id',
+        destination: '/new-blog/:id',
+        permanent: false
+      },
+    ]
+  }
 }
+
+//! COMPLEX PATH MATCHING REDIRECTS (NEXT.JS DOCS)
 
 module.exports = nextConfig
