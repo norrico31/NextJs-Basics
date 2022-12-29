2d3ba8c1c0812d3d2c52ee36f580303066aeebae
norrico31 <norricobiason31@gmail.com>
2022-12-29 17:01:59 +0800

    public and private env

--- my-app/pages/blog/[blogId].tsx
+++ my-app/pages/blog/[blogId].tsx
@@ -7,11 +7,17 @@ export default function Blog({ title, description }: { title: string, descriptio
                 <title>{title}</title>
                 <meta name='description' content={description} />
             </Head>
+            <div>
+                public env - {process.env.NEXT_PUBLIC_SAMPLE}
+            </div>
         </>
     )
 }
 
 export async function getServerSideProps() {
+    const user = process.env.DB_USER
+    const pass = process.env.DB_PASSWORD
+    console.log(user, pass)
     return {
         props: {
             title: 'NextJs Tutorial',
