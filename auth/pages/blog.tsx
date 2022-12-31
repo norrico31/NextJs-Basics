49055eecddc7f75e655064e24e8877b0d228ea96
norrico31 <norricobiason31@gmail.com>
2022-12-31 10:18:10 +0800

    ternary gods

--- auth/pages/blog.tsx
+++ auth/pages/blog.tsx
@@ -4,16 +4,25 @@ export default function Blog({ data }: any) {
     const { data: session } = useSession()
     console.log(session)
     return (
-        <div>Blog page - {data}</div>
+        <div style={{ minHeight: '100vh' }}>
+            Blog page - {data}
+        </div>
     )
 }
 
 export async function getServerSideProps(ctx: any) {
     const session = await getSession(ctx)
     return {
-        props: {
-            session,
-            data: session ? 'List of Private blogs' : 'List of public blogs'
+        ...(session == null) ? {
+            redirect: {
+                destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+                permanent: false
+            }
+        } : {
+            props: {
+                session,
+                data: session ? 'List of Private blogs' : 'List of public blogs'
+            }
         }
     }
 }
