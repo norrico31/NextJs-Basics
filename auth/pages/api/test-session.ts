0440d8fa0bba4ec9bf1763840bb6224ee91072e5
norrico31 <norricobiason31@gmail.com>
2022-12-31 10:25:46 +0800

    securing api route

---
+++ auth/pages/api/test-session.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const session = await getSession({ req })
+    if (!session) {
+        res.status(401).json({ error: 'Unauthorized User' })
+    } else {
+        res.status(200).json({ message: 'Success', data: session })
+    }
+}
